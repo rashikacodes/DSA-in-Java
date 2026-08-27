1a99e057ba1a0836c68caecd24c0f99b459e7672
Rashika Shah <rashikashah0310@gmail.com>
2026-08-27 10:36:41 +0530

    Time: 3 ms (32.45%), Space: 49.6 MB (5.33%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    private final String[] map;
+
+    public Solution() {
+        map = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    }
+    private void helper(String digits, List<String> ans, int index, String current) {
+        if (index == digits.length()) {
+            ans.add(current);
+            return;
+        }
+        String s = map[digits.charAt(index) - '0'];
+        for (int i = 0; i < s.length(); i++) {
+            helper(digits, ans, index + 1, current + s.charAt(i));
+        }
+    }
+    public List<String> letterCombinations(String digits) {
+        List<String> ans = new ArrayList<>(); 
+        if (digits.length() == 0) return ans;
+        helper(digits, ans, 0, "");
+        return ans; 
+    }
+
+}
